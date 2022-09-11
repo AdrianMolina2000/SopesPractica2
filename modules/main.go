287acb13a021f27d175b73e6130af4f7bdfd747e
Adrian Molina <adriansmc@gmail.com>
2022-09-11 17:36:23 -0600

    s

---
+++ modules/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("Datos obtenidos desde el Modulo: ")
+	fmt.Println("")
+
+	cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	output := string(out[:])
+	fmt.Println(output)
+}
