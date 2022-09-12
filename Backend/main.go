2700d3f10b4e47e30fa4127ec18960c47c2d20e5
Adrian Molina <adriansmc@gmail.com>
2022-09-11 19:39:54 -0600

    s

--- Backend/main.go
+++ Backend/main.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
 	"database/sql"
+	"fmt"
 	"log"
+
+	_ "os/exec"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(34.133.39.78:3306)/sopesDB")
-    defer db.Close()
+	defer db.Close()
 
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var version string
-
-    err2 := db.QueryRow("SELECT VERSION()").Scan(&version)
-
-    if err2 != nil {
-        log.Fatal(err2)
-    }
-
-    fmt.Println(version)
-	
-	
-	/*
-	fmt.Println("Datos obtenidos desde el Modulo: ")
-	fmt.Println("")
-
-	cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
-	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	output := string(out[:])
-	fmt.Println(output)
-    */
+
+	var version string
+
+	err2 := db.QueryRow("SELECT VERSION()").Scan(&version)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println(version)
+
+	/*
+		fmt.Println("Datos obtenidos desde el Modulo: ")
+		fmt.Println("")
+
+		cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+		output := string(out[:])
+		fmt.Println(output)
+	*/
 }
