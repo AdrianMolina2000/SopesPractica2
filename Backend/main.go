967ca7b4e16855a499cf11d7c1522f75f687b28f
Adrian Molina <adriansmc@gmail.com>
2022-09-11 19:38:18 -0600

    ss

--- Backend/main.go
+++ Backend/main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"os/exec"
-
-	"github.com/gorilla/mux"
+	"database/sql"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(34.133.39.78:3306)/sopesDB")
+    defer db.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var version string
+
+    err2 := db.QueryRow("SELECT VERSION()").Scan(&version)
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }
+
+    fmt.Println(version)
+	
+	
+	/*
 	fmt.Println("Datos obtenidos desde el Modulo: ")
 	fmt.Println("")
 
-	bd, err := getDB()
-	if err != nil {
-		log.Printf("Error en la BDD: " + err.Error())
-		return
-	} else{
-		err = bd.Ping()
-		if err != nil {
-			log.Printf("Error al conectar con la BDD: " + err.Error())
-			return
-		}
-	}
-
-	router := mux.NewRouter()
-	
-	
-	
-	
-		/*cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
+	cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	output := string(out[:])
 	fmt.Println(output)
-	*/
+    */
 }
