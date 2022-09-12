870aec15a16043e071e3a8730aaae41897cd5684
Adrian Molina <adriansmc@gmail.com>
2022-09-11 19:24:54 -0600

    mrda

--- Backend/main.go
+++ Backend/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os/exec"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Datos obtenidos desde el Modulo: ")
 	fmt.Println("")
 
-	cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
+	bd, err := getDB()
+	if err != nil {
+		log.Printf("Error en la BDD: " + err.Error())
+		return
+	} else{
+		err = bd.Ping()
+		if err != nil {
+			log.Printf("Error al conectar con la BDD: " + err.Error())
+			return
+		}
+	}
+
+	router := mux.NewRouter()
+	
+	
+	
+	
+		/*cmd := exec.Command("sh", "-c", "cat /proc/ram_201903850")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
@@ -17,4 +39,5 @@ func main() {
 
 	output := string(out[:])
 	fmt.Println(output)
+	*/
 }
